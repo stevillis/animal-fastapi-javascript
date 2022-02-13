e91503432326727aa48fa120c56e9d728741488b
Stévillis Sousa <stevillis@hotmail.com>
2022-02-13 01:36:25 -0400

    Fetch All Animals

--- public/index.js
+++ public/index.js
@@ -1,7 +1,20 @@
-function fetchAnimals() {
-  axios.get('http://localhost:8000/animais').then(response => {
-    console.log(response.data);
-  })
+async function fetchAnimals() {
+  const response = await axios.get("http://localhost:8000/animais");
+  const animals = response.data;
+
+  const animalTableBody = document.querySelector("#animalTableBody");
+  animals.forEach((animal) => {
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
+    <td>${animal.id}</td>
+    <td>${animal.name}</td>
+    <td>${animal.age}</td>
+    <td>${animal.sex}</td>
+    <td>${animal.color}</td>
+    `;
+    animalTableBody.appendChild(tr);
+  });
 }
 
 function app() {
