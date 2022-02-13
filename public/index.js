78a766590530c444bb7fd07880ce81329e8cac37
Stévillis Sousa <stevillis@hotmail.com>
2022-02-13 02:13:32 -0400

    Create Animal

--- public/index.js
+++ public/index.js
@@ -1,8 +1,20 @@
-async function fetchAnimals() {
-  const response = await axios.get("http://localhost:8000/animais");
-  const animals = response.data;
+const GET_AND_POST_ENDPOINT = "http://localhost:8000/animais";
 
+async function removeElementChildren(element) {
+  let child = element.lastElementChild;
+
+  while (child) {
+    element.removeChild(child);
+    child = element.lastElementChild;
+  }
+}
+
+async function fetchAnimals() {
+  const response = await axios.get(GET_AND_POST_ENDPOINT);
+  const animals = response.data;
   const animalTableBody = document.querySelector("#animalTableBody");
+
+  await removeElementChildren(animalTableBody);
   animals.forEach((animal) => {
     const tr = document.createElement("tr");
 
@@ -17,7 +29,39 @@ async function fetchAnimals() {
   });
 }
 
+async function createAnimal(name, age, sex, color) {
+  await axios.post(GET_AND_POST_ENDPOINT, {
+    name: name,
+    age: age,
+    sex: sex,
+    color: color
+  });
+  fetchAnimals();
+}
+
+function formSubmit() {
+  const formAnimal = document.querySelector("#formAnimal");
+  const nameInput = document.querySelector("#name");
+  const ageInput = document.querySelector("#age");
+  const sexInput = document.querySelector("#sex");
+  const colorInput = document.querySelector("#color");
+
+  formAnimal.onsubmit = event => {
+    event.preventDefault();
+
+    const name = nameInput.value;
+    const age = parseInt(ageInput.value);
+    const sex = sexInput.value;
+    const color = colorInput.value;
+
+    createAnimal(name, age, sex, color);
+  }
+}
+
 function app() {
+  formSubmit();
+
+  // API 
   fetchAnimals();
 }
 
